78cb7dd4bffadcc855212e48d2befb3e8d71b7eb
Cesar Castillo <al.cs.cesar@gmail.com>
2026-02-25 00:12:52 -0600

    get SparkSession in unity_entities.py

--- modules/io/unity_entities.py
+++ modules/io/unity_entities.py
@@ -1,6 +1,7 @@
+# from databricks.sdk.runtime import *
 from typing import Callable
 from functools import cached_property
-from pyspark.sql import DataFrame, Column
+from pyspark.sql import DataFrame, Column, SparkSession
 from pyspark.sql.functions import col, add_months, lit
 
 ################ Import App Dependencies ################
@@ -11,6 +12,8 @@ def register_project_root():
     if project_root not in sys.path:
         sys.path.append(project_root) 
 
+register_project_root()
+
 from modules.io.contracts import InputTrips, SaveTrips
 
 #########################################################
@@ -18,23 +21,25 @@ from modules.io.contracts import InputTrips, SaveTrips
 ################ RESOURCES ##############################
 
 class UnityTable(InputTrips, SaveTrips):
-    def __init__(self, table_name: str):
+    def __init__(self, table_name: str, spark: SparkSession = None):
         self.table_name = table_name
+        self.spark = SparkSession.builder.getOrCreate()
 
     @cached_property
     def data(self) -> DataFrame:
-        return spark.read.table(self.table_name)
+        return self.spark.read.table(self.table_name)
 
     def save(self, trips: DataFrame, mode: str = "append"):
         trips.write.mode(mode).saveAsTable(self.table_name)
 
 class SchemedCsv(InputTrips, SaveTrips):
-    def __init__(self, qualified_name: str):
+    def __init__(self, qualified_name: str, spark: SparkSession = None):
         self.qualified_name = qualified_name
+        self.spark = SparkSession.builder.getOrCreate()
 
     @cached_property
     def data(self) -> DataFrame:
-        return spark.read.format("csv").options(header= True, inferSchema = True).load(self.qualified_name)
+        return self.spark.read.format("csv").options(header= True, inferSchema = True).load(self.qualified_name)
 
     def save(self, trips: DataFrame, mode: str = "append"):
         trips.write.mode(mode).format("csv").save(self.qualified_name)
